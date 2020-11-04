969e8a93685600131a8e3e931ad5523442f0632f
Menno <menno.vanhout@universal-games.nl>
2020-11-04 17:30:58 +0100

    Update bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -36,8 +36,8 @@ window.Pusher = require('pusher-js');
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
-    wsHost: process.env.MIX_PUSHER_HOST,
-    wsPort: 6001,
+    wsHost: window.location.hostname,
+    wsPort: process.env.MIX_PISHER_PORT,
     forceTLS: process.env.MIX_PUSHER_SCHEME === 'https',
     disableStats: true,
     authorizer: (channel, options) => {
